fb1d45306a093dff78049f8b68b11ba952da457a
CoderZ <adzohonouapelete@gmail.com>
2023-02-20 11:40:03 +0000

    initialising with react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementsByTagName('html'));
+root.render(
+    <React.StrictMode>
+        <h1>
+            Home
+        </h1>
+    </React.StrictMode>
+);
